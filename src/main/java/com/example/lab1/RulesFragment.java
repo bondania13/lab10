33a6dfae8d0d84c6b4e7880a5a6207c6816e94b1
Anna <mlokmlo2@gmail.com>
2020-12-18 19:54:37 +0200

    Initial commit

---
+++ src/main/java/com/example/lab1/RulesFragment.java
@@ -0,0 +1,56 @@
+package com.example.lab1;
+
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.app.Fragment;
+
+import androidx.lifecycle.LifecycleObserver;
+
+import timber.log.Timber;
+
+public class RulesFragment extends Fragment implements LifecycleObserver {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Timber.i("RulesFragment onCreate called");
+
+        final View view = inflater.inflate(R.layout.rules_fragment, container, false);
+
+        return view;
+    };
+    @Override
+    public void onStart() {
+        super.onStart();
+        Timber.i("RulesFragment onStart called");
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Timber.i("RulesFragment onResume called");
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        Timber.i("RulesFragment onPause called");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        Timber.i("RulesFragment onStop called");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Timber.i("RulesFragment onDestroy called");
+    }
+}
+
